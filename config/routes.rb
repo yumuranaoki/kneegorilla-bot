fa0961d5a21264764528bbe39365a132fdaaf5b5
Naoki Yumura <naokiyumura@NaokinoMacBook-ea.local>
2019-01-23 13:48:44 +0900

    modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/', to: 'message#index'
+  get '/', to: 'messages#index'
   post '/message', to: 'messages#create'
 end
